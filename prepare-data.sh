414789ab861fee88b728f2038ef2e8bd47570251
Yikai Zhao <yikai@z1k.dev>
2025-03-22 08:34:43 +0800

    add apple domain list

--- prepare-data.sh
+++ prepare-data.sh
@@ -3,6 +3,9 @@
 cd "$(dirname "$0")"
 
 curl -L -o data/GeoLite2-Country.mmdb https://git.io/GeoLite2-Country.mmdb
+
 curl -L -o - https://github.com/felixonmars/dnsmasq-china-list/raw/refs/heads/master/accelerated-domains.china.conf \
      | sed -E 's/server=\/([^\/]+)\/.*/\1/' > data/china-domain-list.txt
+curl -L -o - https://github.com/felixonmars/dnsmasq-china-list/raw/refs/heads/master/apple.china.conf \
+     | sed -E 's/server=\/([^\/]+)\/.*/\1/' >> data/china-domain-list.txt
 
